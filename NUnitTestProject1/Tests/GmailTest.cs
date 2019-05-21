ca389055881aaf0b721d3ededdb730917757e2c2
QAMAGIC <magicqa1234@outlook.com>
2019-05-21 17:37:53 +0530

    commit

---
+++ NUnitTestProject1/Tests/GmailTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NUnitFramework.Common;
+using NUnitFramework.Pages;
+using NUnitFramework.Utils;
+
+namespace NUnitFramework.Tests
+{
+    class GmailTest : TestBase
+    {
+        public GmailTest() : base()
+        {
+
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("This is to test");
+        }
+
+        [Test]
+        public void GmailLogin()
+        {
+            string emailID;
+            string password;
+            bool testResult;
+            emailID = TestData.GetData("User_14EmailAddress");
+            password = TestData.GetData("GmailUser_Test1Password");
+            //subject = TestData.GetData("GmailMailSubject_ConfirmYourWithdraw");
+            GmailPage gmailObj = new GmailPage(TestProgressLogger);
+            testResult = gmailObj.Gmail(emailID, password, "Withdraw");
+            Assert.True(testResult);
+        }
+
+        [TearDown]
+        public void End()
+        {
+            driver.Quit();
+        }
+    }
+}
